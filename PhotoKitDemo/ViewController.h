ea55e8f23009d589586138824255953cdc6c7108
cAibDe <you@example.com280206014@qq.com>
2017-06-12 09:25:11 +0800

    Initial Commit

---
+++ PhotoKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoKitDemo
+//
+//  Created by 张鹏 on 2017/6/12.
+//  Copyright © 2017年 张鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhotoKitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhotoKitDemo
+//
+//  Created by 张鹏 on 2017/6/12.
+//  Copyright © 2017年 张鹏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
